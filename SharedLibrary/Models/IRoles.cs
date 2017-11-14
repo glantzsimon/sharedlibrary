82f3d9022d7edd68bff66f4067a2aacbda3cf413
Simon Glantz <simon.glantz@mac.com>
2017-11-14 23:54:02 +0000

    sg - extended IRoles

--- SharedLibrary/Models/IRoles.cs
+++ SharedLibrary/Models/IRoles.cs
@@ -4,7 +4,6 @@ namespace K9.SharedLibrary.Models
 {
 	public interface IRoles
 	{
-
 		List<IRole> GetRolesForUser(string username);
 		List<IPermission> GetPermissionsForUser(string username);
 		IRole GetRole(string roleName);
@@ -12,10 +11,13 @@ namespace K9.SharedLibrary.Models
 		bool CurrentUserHasPermissions<T>(params string[] permissionNames) where T : IObjectBase;
 		List<IPermission> GetPermissionsForCurrentUser();
 		List<IRole> GetRolesForCurrentUser();
-		bool UserHasPermissions(string username, params string[] permissionNames);
+	    IPermission GetPermission(string permissionName);
+        bool UserHasPermissions(string username, params string[] permissionNames);
 		bool UserIsInRoles(string username, params string[] roleNames);
-		void CreateRole(string roleName, bool isSystemStandard = false);
+	    bool PermissionIsInRole(string permissionName, string roleName);
+        void CreateRole(string roleName, bool isSystemStandard = false);
 		void CreatePermission(string permissionName, bool isSystemStandard = false);
 		void AddUserToRole(string username, string roleName);
+	    void AddPermissionsToRole(string permissionName, string roleName);
 	}
 }

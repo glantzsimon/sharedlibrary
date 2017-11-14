618db60a0d501bfe4d10f41d3f8fa231e5c27f55
Simon Glantz <simon.glantz@mac.com>
2017-11-14 21:01:51 +0000

    sg - updated datasets to include nameExpression

--- SharedLibrary/Models/IDataSetsHelper.cs
+++ SharedLibrary/Models/IDataSetsHelper.cs
@@ -5,9 +5,9 @@ namespace K9.SharedLibrary.Models
 {
 	public interface IDataSetsHelper
 	{
-		List<ListItem> GetDataSet<T>(bool refresh = false) where T : class, IObjectBase;
+		List<ListItem> GetDataSet<T>(bool refresh = false, string nameExpression = "Name") where T : class, IObjectBase;
 		List<ListItem> GetDataSetFromEnum<T>(bool refresh = false);
-		SelectList GetSelectList<T>(int? selectedId, bool refresh = false) where T : class, IObjectBase;
+		SelectList GetSelectList<T>(int? selectedId, bool refresh = false, string nameExpression = "Name") where T : class, IObjectBase;
 		SelectList GetSelectListFromEnum<T>(int selectedId, bool refresh = false);
 		string GetName<T>(int? selectedId, bool refresh = false) where T : class, IObjectBase;
 	}
